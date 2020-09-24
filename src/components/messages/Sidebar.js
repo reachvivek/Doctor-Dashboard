bc68a410f2e5b4501dff43ab9a141db7811e0929
Vivek Kumar Singh <rogerthatvivek@gmail.com>
2020-09-25 05:23:24 +0530

    UI Tweaks (Messages UI)

--- src/components/messages/Sidebar.js
+++ src/components/messages/Sidebar.js
@@ -2,13 +2,19 @@ import React from 'react';
 import "./sidestyle.css";
 import SidebarChat from "./SidebarChat";
 import {IconButton} from "@material-ui/core";
-import {Settings, SearchOutlined} from "@material-ui/icons/";
+import {Settings, SearchOutlined, Create, Fullscreen, AddIcCall, NearMe} from "@material-ui/icons/";
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
                 <div className="sidebar__headerRight">
+                    <IconButton>
+                        <Create/>
+                    </IconButton>
+                    <IconButton>
+                        <NearMe/>
+                    </IconButton>
                     <IconButton>
                         <Settings/>
                     </IconButton>
