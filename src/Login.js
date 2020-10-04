8371b7e392f5f1f34c559850ee1b9854ef3a471a
Vivek Kumar Singh <rogerthatvivek@gmail.com>
2020-10-04 16:10:58 +0530

    Updated Logo and Meta Tags

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Login.css'
-import Logo from "./images/logos/stethoscope.svg" 
+import Logo from "./images/logos/logo.png" 
 import { Button } from '@material-ui/core'
 import {auth, provider } from './firebase';
 import { actionTypes } from './reducer';
